ae12a7f85da82f6c7819c24eb4cfec1ba7440495
StevenJohnstone <sjohnstone1986@googlemail.com>
2019-11-07 15:48:42 +0000

    Initial Commit

---
+++ StudentShare.API/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentShare.API.Data;
+
+namespace StudentShare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues() // Async to allow more than one user to access at a time
+        {
+            var values = await _context.Values.ToListAsync(); //goes and gets values from the database as a list
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id) // Async to allow more than one user to access at a time
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id); //goes and gets only the requested value and stores in value
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ StudentShare.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using StudentShare.API.Models;
+
+namespace StudentShare.API.Data
+{
+    public class DataContext : DbContext // DataContext inherits from DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){     
+        }
+
+        public DbSet<Value> Values {get; set;}
+    }
+}
