0a2d7852eaf35abe3f954457d510721f7cf86516
StevenJohnstone <sjohnstone1986@googlemail.com>
2019-11-08 14:58:58 +0000

    Security Commit

---
+++ StudentShare.API/Controllers/AuthController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using StudentShare.API.Data;
+using StudentShare.API.Dtos;
+using StudentShare.API.Models;
+
+namespace StudentShare.API.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repo, IConfiguration config)
+        {
+            _repo = repo;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            return BadRequest("Username already exists.");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password); //checks username + password matches info held in db
+
+            if(userFromRepo==null)
+                return Unauthorized();
+
+            var claims = new[] //token contains 2 claims - a username and id
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8 //creates the security key for the user
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); //hashes the key
+
+            var tokenDescriptor = new SecurityTokenDescriptor // creates the token
+            {
+                Subject = new ClaimsIdentity(claims), //passes the claims from above
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler(); 
+
+            var token = tokenHandler.CreateToken(tokenDescriptor); //creates the JWT for the tokendescripter above
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token) //when its ok, it writes the token 
+            });
+        }
+    }
+
+}
---
+++ StudentShare.API/Data/AuthRepository.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentShare.API.Models;
+
+namespace StudentShare.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context; //lets us view the DB data - hence read only
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<User> Login(string username, string password) //async to make it possible for many users to do at the same time
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username); //used to find username that matches or return null
+
+            if (user == null)
+                return null;
+            
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt)) 
+                return null;
+
+            return user;
+        
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for(int i = 0; i < computedHash.Length; i++) // looping through hash to make sure they match
+                {
+                    if (computedHash[i] != passwordHash[i]) 
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public async Task<User> Register(User user, string password) //async to make it possible for many users to do at the same time
+        {
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash(password,out passwordHash,out passwordSalt); //when hash and salt are created, they are auto updated with the out's
+ 
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user); 
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            if (await _context.Users.AnyAsync(x => x.Username == username)) //checks db to see if user exists
+                return true;
+
+            return false;
+        }
+    }
+}
