160b1330d8390fe784dd4e362730cce8bcbb1d9e
StevenJohnstone <sjohnstone1986@googlemail.com>
2019-11-11 22:12:10 +0000

    Initial Angular Creation

--- StudentShare.API/Controllers/ValuesController.cs
+++ StudentShare.API/Controllers/ValuesController.cs
@@ -20,6 +20,7 @@ namespace StudentShare.API.Controllers
             _context = context;
 
         }
+        [AllowAnonymous]
         // GET api/values
         [HttpGet]
         public async Task<IActionResult> GetValues() // Async to allow more than one user to access at a time
